39f7f20b52af1202c8329ffb330b38d52103385e
Matthew Voss <matthewpvoss@gmail.com>
2024-08-13 06:42:03 -0500

    Conditional supabase instantiation

--- frontend/src/utils/supabase.ts
+++ frontend/src/utils/supabase.ts
@@ -1,10 +1,14 @@
 
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
-const supabase = createClient(supabaseUrl, supabaseKey);
+let supabase;
 
+if (import.meta.env.VITE_APP_MODE != 'docker') {
+    const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+    const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+
+    supabase = createClient(supabaseUrl, supabaseKey);
+}
 export default supabase
         
